b29764285dcfb913178168f019f4c7404d0c6ecc
Abubeker Shamil <abubekershamil123@gmail.com>
2024-08-01 08:41:17 +0000

    doc: add docstring for easier redablity

--- scripts/testset_generator.py
+++ scripts/testset_generator.py
@@ -5,33 +5,53 @@ from ragas.testset.evolutions import simple, reasoning, multi_context
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 import pandas as pd
 
-def generate_testset(test_size=6):
+def generate_testset(test_size: int = 6) -> pd.DataFrame:
+    """
+    Generate a testset using the TestsetGenerator class from the ragas package.
 
-    dataset = load_dataset("cnn_dailymail", "3.0.0", split="validation[:1000]", cache_dir="./cache")
+    Args:
+        test_size (int): The number of test samples to generate. Default is 6.
 
-    articles = [Document(page_content=item['article'], metadata={"id": item['id']}) for item in dataset]
+    Returns:
+        pd.DataFrame: The generated testset as a pandas DataFrame.
+    """
+    dataset = load_dataset(
+        "cnn_dailymail", "3.0.0", split="validation[:1000]", cache_dir="./cache"
+    )
+
+    articles = [
+        Document(page_content=item["article"], metadata={"id": item["id"]})
+        for item in dataset
+    ]
 
     article_slice = articles[200:250]
 
-    # generator with openai models
-    generator_llm = critic_llm = ChatOpenAI(model="gpt-3.5-turbo-16k")
-    # critic_llm = ChatOpenAI(model="gpt-4") # uncomment to use gpt-4
+    # Generator with OpenAI models
+    generator_llm = ChatOpenAI(model="gpt-3.5-turbo-16k")
+
+    critic_llm = ChatOpenAI(model="gpt-4-turbo")
+
     embeddings = OpenAIEmbeddings()
 
     generator = TestsetGenerator.from_langchain(
-        generator_llm,
-        critic_llm,
-        embeddings
+        generator_llm, critic_llm, embeddings
     )
 
-    # generate testset
-    testset = generator.generate_with_langchain_docs(article_slice, test_size=test_size, distributions={simple: 0.33, reasoning: 0.34, multi_context: 0.33})
+    # Generate testset
+    testset = generator.generate_with_langchain_docs(
+        article_slice,
+        test_size=test_size,
+        distributions={simple: 0.33, reasoning: 0.34, multi_context: 0.33},
+    )
 
     synthetic = testset.to_pandas()
 
     timestamp = pd.Timestamp.now().strftime("%Y%m%d-%H%M%S")
 
-    synthetic.to_csv(f"/teamspace/studios/this_studio/RAG-System-Optimization/data/{timestamp}testset.csv", index=False)
+    synthetic.to_csv(
+        f"/teamspace/studios/this_studio/RAG-System-Optimization/data/{timestamp}testset.csv",
+        index=False,
+    )
 
     return synthetic
 
